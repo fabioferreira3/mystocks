bbc83eb288d02620cf8f31998a577c20519c0c4d
Fabio Ferreira <fabioferreira@Mac-mini.local>
2021-06-06 01:33:28 -0300

    fixed month result calculation

--- app/Domain/Stock/Services/CalculateMonthlyResult.php
+++ app/Domain/Stock/Services/CalculateMonthlyResult.php
@@ -6,6 +6,7 @@ use Carbon\Carbon;
 use Domain\Stock\Events\StockTransactionCreated;
 use Domain\Stock\Helpers\StockHelper;
 use Domain\Stock\MonthlyResult;
+use Illuminate\Support\Facades\Log;
 
 class CalculateMonthlyResult {
 
@@ -16,32 +17,27 @@ class CalculateMonthlyResult {
         $endOfMonthDate = $parsedDate->endOfMonth()->format('Y-m-d');
         $monthResult = MonthlyResult::byDate($endOfMonthDate);
         $stockValue = StockHelper::calculateStockValue($transactionData['amount'], $transactionData['unit_price']);
-        $stockTotalValue = $stockValue + $transactionData['taxes'];
 
-        if($transactionData['type'] == 'buy') {
+        if ($transactionData['type'] == 'buy') {
             $stockValue = $stockValue * -1;
-            $stockTotalValue = $stockTotalValue * -1;
         }
+
+        $stockTotalValue = $stockValue - $transactionData['taxes'];
 
         if (!$monthResult) {
             return MonthlyResult::create([
                 'total_value' => $stockValue,
                 'month_result' => $stockTotalValue,
                 'taxes' => $transactionData['taxes'],
-                'hoof_gain' => (bool) $stockTotalValue > 20000,
+                'hoof_gain' => $stockTotalValue > 20000,
                 'at_date' => $endOfMonthDate,
             ]);
         }
 
-        if ($transactionData['type'] == 'buy') {
-            $monthResult->total_value += $stockTotalValue;
-        } else {
-            $monthResult->total_value -= $stockTotalValue;
-        }
-
+        $monthResult->total_value += $stockValue;
         $monthResult->month_result += $stockTotalValue;
         $monthResult->taxes += $transactionData['taxes'];
-        $monthResult->hoof_gain = (bool) $monthResult->total_value > 20000;
+        $monthResult->hoof_gain = $monthResult->total_value > 20000;
         $monthResult->save();
     }
 }
