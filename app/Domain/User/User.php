931ce59ec9516030c23ff37bbe89ae9e72f8c0da
Fabio Ferreira <fabio.ferreira3@outlook.com>
2021-08-08 22:56:32 -0300

    add user

--- app/Domain/User/User.php
+++ app/Domain/User/User.php
@@ -11,7 +11,7 @@ use Illuminate\Support\Str;
 use Laravel\Sanctum\HasApiTokens;
 use Spatie\Permission\Traits\HasRoles;
 
-class User extends Authenticatable
+class User extends Authenticatable implements MustVerifyEmail
 {
     use HasApiTokens, HasFactory, HasRoles, Notifiable;
 
